e946014456cdab24015efcf241a2ee1473fb1a63
bidyanandme <ubidyanand@gmail.com>
2024-03-07 15:16:40 +0545

    initail prj

---
+++ angular-16-jwt-auth-master/angular-16-jwt-auth-master/src/app/_services/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const AUTH_API = 'https://localhost:44324/api/Auth/';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post('Access-Control-Allow-Origin: https://localhost:44324/api/Auth/signin?User_ID=admin&Password=admin' ,'Vary: Origin',
+      httpOptions
+    );
+  }
+
+  register(username: string, email: string, password: string): Observable<any> {
+    return this.http.post(
+      AUTH_API + 'signup',
+      {
+        username,
+        email,
+        password,
+      },
+      httpOptions
+    );
+  }
+
+  logout(): Observable<any> {
+    return this.http.post(AUTH_API + 'signout', { }, httpOptions);
+  }
+}
